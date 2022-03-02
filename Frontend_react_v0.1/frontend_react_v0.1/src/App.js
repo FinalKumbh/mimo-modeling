e569d4cd3a41e9e2bd40d25fc754dce1546634b4
Peter <pansekjung@gmail.com>
2022-03-02 13:06:03 +0900

    frontend_v01

---
+++ Frontend_react_v0.1/frontend_react_v0.1/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Camera, Footer, Header, Main} from './container';
+import { Navbar } from './components';
+
+import './App.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <Camera />
+      <Footer />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
